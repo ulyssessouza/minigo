c6613ca22414ccba09815166dd70af3511456308
Ulysses Souza <ulyssessouza@gmail.com>
2024-12-09 11:51:57 +0100

    Add NX
    Signed-off-by: Ulysses Souza <ulyssessouza@gmail.com>

--- minigo.go
+++ minigo.go
@@ -1,8 +1,53 @@
 package minigo
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-var _S = fmt.Sprintf
-var _F = fmt.Fprintf
-var _P = fmt.Printf
-var _E = fmt.Errorf
+var S = fmt.Sprintf
+var SN = fmt.Sprintln
+
+var F = fmt.Fprintf
+var FN = fmt.Fprintln
+
+var P = fmt.Printf
+var PN = fmt.Println
+
+var E = fmt.Errorf
+
+func FE(format string, err error) {
+	if err != nil {
+		log.Fatalf(format, err)
+	}
+}
+
+func N1[T any](first T, rest ...any) T {
+	if len(rest) > 0 {
+		err, ok := rest[0].(error)
+		if ok && err != nil {
+			log.Fatal(err)
+		}
+	}
+	return first
+}
+
+func N2[T any](_, second T, rest ...any) T {
+	if len(rest) > 0 {
+		err, ok := rest[0].(error)
+		if ok && err != nil {
+			log.Fatal(err)
+		}
+	}
+	return second
+}
+
+func N3[T any](_, _ any, third T, rest ...any) T {
+	if len(rest) > 0 {
+		err, ok := rest[0].(error)
+		if ok && err != nil {
+			log.Fatal(err)
+		}
+	}
+	return third
+}
